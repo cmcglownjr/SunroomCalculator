c006cfca55ddee32d25a8560a90745af329e5e78
Chris McGlown <cmcglown@tutamail.com>
2021-02-10 10:44:44 -0500

    I tried for a while to use Uno Platform as the GUI but couldn't get it to work. I'll use Avalonia because I know it.

---
+++ SunroomCalculatorAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace SunroomCalculatorAvalonia.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
